fa35701e3b915e69b7fa535c16a053fc6903c3fa
HarikeshPratapSingh <harikeshpratapsinghwork@gmail.com>
2025-04-18 16:53:21 +0530

    added file router

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
 from controllers.auth_controller import router as auth_router
-
+from controllers.file_controller import router as file_router
 load_dotenv()
 
 app = FastAPI()
 
 # Include Auth Controller
 app.include_router(auth_router)
+app.include_router(file_router)
 
 @app.get("/")
 def root():
