a68ddf2fcf10babfef9a0816dfa718a28576bc5e
HarikeshPratapSingh <harikeshpratapsinghwork@gmail.com>
2025-04-18 16:23:11 +0530

    added file get and upload

--- backend/models/database_model.py
+++ backend/models/database_model.py
@@ -3,6 +3,8 @@ from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
 from typing import Any, Dict, Optional, List
 import os
+from gridfs import GridFS
+from bson import ObjectId
 
 load_dotenv()
 
@@ -51,4 +53,34 @@ class MongoDB:
             return list(collection.find(query))
         except errors.PyMongoError as e:
             print("❌ Find Many Error:", e)
+            raise
+    
+    @classmethod
+    def insert_file(cls, db_name: str, file_path: str, filename: Optional[str] = None, metadata: Optional[Dict[str, Any]] = None) -> ObjectId:
+        try:
+            cls._connect()
+            db = cls._client[db_name]
+            fs = GridFS(db)
+            with open(file_path, "rb") as file:
+                file_id = fs.put(file, filename=filename or os.path.basename(file_path), metadata=metadata)
+                print(f"✅ File '{filename or os.path.basename(file_path)}' inserted with id: {file_id}")
+                return file_id
+        except Exception as e:
+            print("❌ File Insert Error:", e)
+            raise
+
+    @classmethod
+    def get_file(cls, db_name: str, file_id: ObjectId, output_path: Optional[str] = None) -> str:
+        try:
+            cls._connect()
+            db = cls._client[db_name]
+            fs = GridFS(db)
+            grid_out = fs.get(file_id)
+            output_path = output_path or grid_out.filename
+            with open(output_path, "wb") as f:
+                f.write(grid_out.read())
+            print(f"✅ File retrieved and saved as: {output_path}")
+            return output_path
+        except Exception as e:
+            print("❌ File Retrieval Error:", e)
             raise
